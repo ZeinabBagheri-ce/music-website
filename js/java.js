e3228aa2b69c59057c54a487cd6ec1108f5261ed
Zeinab Bagheri <zein.bagheri.ce@gmail.com>
2023-12-24 23:54:06 +0330

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,43 @@
+a = prompt('enter number');
+b = prompt('enter number');
+
+function f1(a,b){
+   r = a + b;
+   return r;
+}
+function f2(){
+    alert(r * 2);
+}
+
+f1(a,b);
+f2();
+
+// c = prompt('enter operator');
+
+// switch(c){
+//     case '+' :
+//         alert(a+b);
+//         break;
+//     case '*' :
+//         alert(a*b);
+//         break;  
+        
+//     case '-' :
+//         alert(a-b);
+//         break;
+
+//     case '/' :
+//         if(b == 0){
+//             alert('enter any number but zero')
+//         }
+//         alert(a/b);
+//         break;
+// }
+
+
+
+// if(a % 2 == 0){
+//     alert("even")
+// }else{
+//     alert("odd")
+// }
